a3b129a23b33920ea579efed6061542485ed568d
Richard Wild <richard.wild@codurance.com>
2021-09-09 15:17:09 +0100

    Refactor the left turn functionality

--- src/mars_rover_clojure/mars_rover.clj
+++ src/mars_rover_clojure/mars_rover.clj
@@ -1,11 +1,13 @@
 (ns mars-rover-clojure.mars-rover)
 
+(def rotations-left {:NORTH :WEST, :WEST :SOUTH, :SOUTH :EAST, :EAST :NORTH})
+
 (def grid {:width 10, :height 10})
 (def obstacles #{})
 
 (defn execute [rover command]
   (let [current-heading (rover :heading)]
-    (assoc rover :heading ({:NORTH :WEST, :WEST :SOUTH, :SOUTH :EAST, :EAST :NORTH} current-heading))))
+    (assoc rover :heading (rotations-left current-heading))))
 
 (defn mars-rover-driver [commands]
   nil)
